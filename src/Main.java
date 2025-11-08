1e6f7ed0b0d02c8fcfcfb75228b290f89d05471e
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2025-11-08 22:33:46 +0300

    array desteklendi
    ali tolazın array çalışması düzeltildi

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,17 @@ import java.awt.*;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] numbers= new int[5,2,4,5,6,7;]
+    public static void main(String[] args) 
+    {
+        //Array (Dizi) Nedir?
+        int[] notlar = new int[];
+        notlar[0] = 76;
+        notlar[1] = 81;
+        System.out.println(notlar[1]);
 
+        int[] numbers = {5,2,4,54,6,7};
+        System.out.println(numbers[3]);
 
         System.out.println(Arrays.toString(numbers));
-
-
-
-
-
-
     }
 }
